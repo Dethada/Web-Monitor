31da2d4f121a3644f12d7634f8a3cf4b599073f9
PotatoDrug <zhy.david@gmail.com>
2018-08-06 13:23:12 +0800

    Continues to monitor after website is down & added pipenv files

--- monitor.py
+++ monitor.py
@@ -15,25 +15,39 @@ page = Path(page_file)
 if not page.is_file():
     with open(page_file, 'w') as f:
         f.write(requests.get(URL).text)
+down = False
+not200 = False
 
 print('Monitoring {} ...'.format(URL))
 
 try:
     while True:
+        time.sleep(FREQUENCY)
         try:
             response = requests.get(URL)
         except Exception as e:
-            mail_admins('{} might be down!'.format(URL), 'Web monitor expirenced an exception at {}\n{}'.format(datetime.now(), str(e)))
-            print('{} might be down! {}\n{}'.format(URL, datetime.now(), str(e)))
-            break
+            if not down:
+                mail_admins('{} might be down!'.format(URL), 'Web monitor expirenced an exception at {}\n{}'.format(datetime.now(), str(e)))
+                print('{} might be down! {}\n{}'.format(URL, datetime.now(), str(e)))
+                down = True
+                continue
+            else:
+                continue
 
         if response.status_code != 200:
-            code = response.status_code
-            mail_admins('{} return status {}!'.format(URL, code), '{} returned {} at {}'.format(URL, code, datetime.now()))
-            print('{} returned status {} at {}'.format(URL, code, datetime.now()))
-            break
+            if not not200:
+                code = response.status_code
+                mail_admins('{} return status {}!'.format(URL, code), '{} returned {} at {}'.format(URL, code, datetime.now()))
+                print('{} returned status {} at {}'.format(URL, code, datetime.now()))
+                not200 = True
+                continue
+            else:
+                continue
 
         with open(page_file, 'r+') as f:
+            if down or not200:
+                mail_admins('{} Updated'.format(URL), '{} returned to normal at {}'.format(URL, datetime.now()))
+                print('{} returned to normal at {}'.format(URL, datetime.now()))
             new = response.text
             old = f.read()
             if new != old:
@@ -45,7 +59,8 @@ try:
                     f2.write(old)
                 mail_admins('{} Updated'.format(URL), '{} was updated at {}'.format(URL, datetime.now()))
                 print('{} updated {} '.format(URL, datetime.now()))
-        
-        time.sleep(FREQUENCY)
+            down = False
+            not200 = False
+
 except KeyboardInterrupt:
     print('\nExiting...')
